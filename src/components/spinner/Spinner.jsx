093115e14ca5fc05aac1aebf56cab31241b90c6a
GuillermoRivera <guillermoriveragallego1999@gmail.com>
2025-07-14 14:22:52 +0200

    refactor

--- src/components/spinner/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-const Spinner = () => (
-  <div className="flex justify-center items-center h-32">
-    <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
-  </div>
-);
-
-export default Spinner;
